d8f6ebdd8c58fe6c9df569b762833f6f456a75af
Scherdin <S1774A@corp1.ad1.seb.net>
2017-08-03 12:48:45 +0200

    hammerjs

--- timereport/src/app/app.module.ts
+++ timereport/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
+import 'hammerjs';
 import { AppComponent } from './app.component';
 
 @NgModule({
